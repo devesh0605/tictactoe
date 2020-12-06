af4e4657bd2eb9ee8ed53335611e0f198053fd76
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-06 19:04:09 +0530

    calculate score

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import Board from './components/Board'
+import { calculateWinner } from "./helpers";
 import './styles/root.scss'
 
 const App = () =>{
+  const [board,setBoard] = useState(Array(9).fill(null));
+  const [isXnext, setIsNext]=useState(false);
+
+  const winner = calculateWinner(board); 
+  const message = winner 
+  ? 'Winner is '+winner
+  : 'Next player is '+(isXnext ? 'X' : 'O');
+  const handleSquareClick = position =>{
+      if (board[position]||winner){
+          return;
+      }
+
+      setBoard((prev)=>{
+          return prev.map((Square,pos)=>{
+              if (pos==position){
+                  return isXnext ? 'X':'O';
+              }
+              return Square;
+
+          })
+      })
+      setIsNext((prev)=>!prev)
+
+  };
   return(
     <div className="app">
+      
     <h1>TIC TAC TOE</h1>
-    <Board/>
+    <h2> {message} </h2>
+    <Board board={board}handleSquareClick={handleSquareClick}/>
     </div>
 
   )
